2d0eeedcfa7582f0f82db5a57559af4969b59e47
Rockchalk8985 <steven.foster10@gmail.com>
2020-09-08 11:10:02 -0500

    done with assignment

--- code.js
+++ code.js
@@ -5,6 +5,12 @@ window.onload = function () { // Run this once the page has loaded.
         const url = "https://api.github.com/search/users?q="
         const searchUserText = document.querySelector("#searchUser").value;
 
+        fetch(url+searchUserText)
+        .then((response) => response.json())
+        .then((result) => {
+            renderUserList(result.items);
+        
+        });
         // Obtain a list of users from the Github API that match searchUserText
         //  The final result will contain an array under the key 'items'
         // Pass this array to `renderUserList`
@@ -16,6 +22,7 @@ window.onload = function () { // Run this once the page has loaded.
         for (let i = 0; i < githubUsers.length; i++) {
             let githubUser = githubUsers[i];
             html += "<li>";
+            html +=`<img style="width: 2rem" src=${githubUser.avatar_url}/>`;
             html += `<strong>${githubUser.login}</strong>`;
             html += `<a target="_blank" href="${githubUser.html_url}">`;
             html += "</li>";
